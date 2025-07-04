d9ba8322c2219eb0be8c68c2d40c252d55909c2c
Waleedfarrukh-code <waleedfarrukh74@gmail.com>
2025-07-04 15:05:06 +0500

    mara ek payara sa message

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text" , {
+    strings : ["frontend developer", "youtuber" ,"web developer"],
+    typespeed : 100,
+    backspeed : 100,
+    backDelay : 1000,
+    loop: true
+});
